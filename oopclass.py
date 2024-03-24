d0615a161234403153200a49e8b479a5cf58af54
GkyEla <60583610+GkyEla@users.noreply.github.com>
2024-03-24 15:14:54 +0000

    Self class upgrade
    self class practice upgraded

--- oopclass.py
+++ oopclass.py
@@ -1,13 +1,32 @@
 class worker():
-    print('Worker class created.')
+    
     appellation = 'personnel'
     work_times = '9am - 5pm'
     
 
     def __init__(self):
+        print('Worker class created.')
         self.skills = []
+        self.wage = 2000
+        self.work_days = 0
+
+mike = worker()
+mike.skills.append('knows ruby')
+mike.wage = 1700
+
+print(mike.wage)
+print(mike.work_times)
 
 
+
+
+
+
+
+
+
+#self class
+'''
 john = worker()
 john.skills.append("knows python")
 print('john`s skills : ',john.skills)
@@ -15,3 +34,4 @@ print('john`s skills : ',john.skills)
 george = worker()
 #george.skills.append('knows c++')
 print('Georges skills: ',george.skills)
+'''
